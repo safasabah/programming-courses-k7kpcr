887365a76cd64fd9680b39a2fb01451ae75e3086
safasabah <safamath2012@gmail.com>
2021-06-29 20:24:00 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -9,3 +9,15 @@ num1 = 76;
 num2 = 122;
 num3 = num1 + num2;
 console.log(num3);
+
+let am1: number;
+let am2: number;
+let am3: number;
+let am4: number;
+let am5: number;
+am1 = 5;
+am2 = 3;
+am3 = 10;
+am4 = 20;
+am5 = am1 + am2 + am3 + am4;
+console.log(am5);
